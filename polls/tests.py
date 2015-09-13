33712969ca5d4a1a82bca904ccccc4229159d313
MrLoh <flojoto@hotmail.com>
2015-09-13 14:42:46 +0200

    finished basic django tutorial

--- polls/tests.py
+++ polls/tests.py
@@ -43,8 +43,7 @@ def create_question(question_text, days):
 	in the past, positive for questions that have yet to be published).
 	"""
 	time = timezone.now() + datetime.timedelta(days=days)
-	return Question.objects.create(question_text=question_text,
-								   pub_date=time)
+	return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionViewTests(TestCase):
@@ -77,8 +76,7 @@ class QuestionViewTests(TestCase):
 		"""
 		create_question(question_text="Future question.", days=30)
 		response = self.client.get(reverse('polls:index'))
-		self.assertContains(response, "No polls are available.",
-							status_code=200)
+		self.assertContains(response, "No polls are available.", status_code=200)
 		self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
 	def test_index_view_with_future_question_and_past_question(self):
@@ -105,3 +103,23 @@ class QuestionViewTests(TestCase):
 			response.context['latest_question_list'],
 			['<Question: Past question 2.>', '<Question: Past question 1.>']
 		)
+
+class QuestionIndexDetailTests(TestCase):
+
+	def test_detail_view_with_a_future_question(self):
+		"""
+		The detail view of a question with a pub_date in the future should
+		return a 404 not found.
+		"""
+		future_question = create_question(question_text='Future question.', days=5)
+		response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+		self.assertEqual(response.status_code, 404)
+
+	def test_detail_view_with_a_past_question(self):
+		"""
+		The detail view of a question with a pub_date in the past should
+		display the question's text.
+		"""
+		past_question = create_question(question_text='Past Question.', days=-5)
+		response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+		self.assertContains(response, past_question.question_text, status_code=200)
--- polls/views.py
+++ polls/views.py
@@ -11,14 +11,16 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.filter(
-			pub_date__lte=timezone.now()
-		).order_by('-pub_date')[:5]
+		"""Return the 5 last published questions."""
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		""" Excludes any questions that aren't published yet."""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultView(generic.DetailView):
